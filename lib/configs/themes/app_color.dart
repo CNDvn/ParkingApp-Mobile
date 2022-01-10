f16c79259e5abcb39c8aeda5587ed7683d47785d
baosphoa0403 <baosphoa0403@gmail.com>
2022-01-10 13:10:47 +0700

    config theme color and style element

---
+++ lib/configs/themes/app_color.dart
@@ -0,0 +1,8 @@
+class AppColor {
+  static final lightButton = Color(#3da9fc);
+  static final blackParagraph = Color(##5f6c7b);
+  static final whiteTextButton  = Color(##fffffe);
+  static final blackHeadline = Color(#094067);
+
+  
+}
---
+++ lib/configs/themes/app_text_style.dart
@@ -0,0 +1,5 @@
+class AppTextStyles {
+  static const h1 = TextStyle(
+    
+  )
+}
