1e9c1084a2bbec01733c2f7dc4a3cf9840a66297
DQLok <wkhhkw01@gmail.com>
2022-01-13 18:02:05 +0700

    process picture done

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -1,4 +1,5 @@
 class AssetPath {
   static const String prePath = "assets/images";
   static const String posterCity = '$prePath/city.png';
+  static const String processWatting = '$prePath/process.json'; 
 }
