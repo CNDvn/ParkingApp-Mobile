8e7400dfe81b89bbb2745f1956ed4a30c99cd350
DuyLP <lp.duy2k@gmail.com>
2022-01-13 16:58:54 +0700

    update welcome page

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -2,7 +2,7 @@ class AssetPath {
   static const String prePath = "assets/images";
   static const String posterCity = '$prePath/city.png';
   static const String logo = '$prePath/logo.png';
-  static const String welcome1 = '$prePath/welcome1.png';
-  static const String welcome2 = '$prePath/welcome2.png';
-  static const String welcome3 = '$prePath/welcome3.png';
+  static const String welcomeOne = '$prePath/welcome1.png';
+  static const String welcomeTwo = '$prePath/welcome2.png';
+  static const String welcomeThree = '$prePath/welcome3.png';
 }
--- lib/view/welcome/intro_slider.dart
+++ lib/view/welcome/intro_slider.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intro_slider/intro_slider.dart';
 import 'package:intro_slider/slide_object.dart';
 import 'package:parkingappmobile/configs/themes/app_color.dart';
-import 'package:parkingappmobile/constants/assets_path.dart';
+import 'package:parkingappmobile/configs/themes/app_text_style.dart';
+import 'package:parkingappmobile/view/welcome/data_slides.dart';
 
 class IntroSliderPage extends StatefulWidget {
   const IntroSliderPage({Key? key}) : super(key: key);
@@ -19,39 +20,19 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
     // TODO: implement initState
     super.initState();
 
-    slides.add(
-      new Slide(
-        title: 'Best Parking Spots',
-        styleTitle: TextStyle(
-            color: AppColor.blackText, fontSize: 24.0, fontWeight: FontWeight.bold),
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        styleDescription: TextStyle(color: AppColor.greyText, fontSize: 18.0),
-        pathImage: '${AssetPath.welcome1}',
-        backgroundColor: AppColor.whiteBackground,
-      ),
-    );
-    slides.add(
-      new Slide(
-        title: 'Quick Navigation',
-        styleTitle: TextStyle(
-            color: AppColor.blackText, fontSize: 24.0, fontWeight: FontWeight.bold),
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        styleDescription: TextStyle(color: AppColor.greyText, fontSize: 18.0),
-        pathImage: '${AssetPath.welcome2}',
-        backgroundColor: AppColor.whiteBackground,
-      ),
-    );
-    slides.add(
-      new Slide(
-        title: 'Easy Payment',
-        styleTitle: TextStyle(
-            color: AppColor.blackText, fontSize: 24.0, fontWeight: FontWeight.bold),
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        styleDescription: TextStyle(color: AppColor.greyText, fontSize: 18.0),
-        pathImage: '${AssetPath.welcome3}',
-        backgroundColor: AppColor.whiteBackground,
-      ),
-    );
+    dataSlides.forEach((slide) => {
+          slides.add(
+            new Slide(
+              title: slide.title,
+              styleTitle: AppTextStyles.h2Black,
+              description: slide.description,
+              styleDescription:
+                  TextStyle(color: AppColor.greyText, fontSize: 18.0),
+              pathImage: slide.pathImage,
+              backgroundColor: AppColor.whiteBackground,
+            ),
+          )
+        });
   }
 
   void onDonePress() {
@@ -59,13 +40,6 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
     //   context,
     //   MaterialPageRoute(builder: (context) => HomePage()),
     // );
-  }
-
-  ButtonStyle myButtonStyle() {
-    return ButtonStyle(
-      shape: MaterialStateProperty.all<OutlinedBorder>(StadiumBorder()),
-      overlayColor: MaterialStateProperty.all<Color>(AppColor.blueBackground),
-    );
   }
 
   @override
@@ -79,21 +53,21 @@ class _IntroSliderPageState extends State<IntroSliderPage> {
         'SKIP',
         style: TextStyle(color: AppColor.blueBackground),
       ),
-      skipButtonStyle: myButtonStyle(),
+      skipButtonStyle: dotButtonStyle(),
 
       // Next button
       renderNextBtn: Text(
         'NEXT',
         style: TextStyle(color: AppColor.blueText),
       ),
-      nextButtonStyle: myButtonStyle(),
+      nextButtonStyle: dotButtonStyle(),
 
       // Done button
       renderDoneBtn: Text(
         'DONE',
         style: TextStyle(color: AppColor.blueText),
       ),
-      doneButtonStyle: myButtonStyle(),
+      doneButtonStyle: dotButtonStyle(),
       onDonePress: this.onDonePress,
 
       // Dot indicator
