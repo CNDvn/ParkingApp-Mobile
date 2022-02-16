2644a5b95b22e9268cb5fc8b7b745dcb84ae9de1
DQLok <wkhhkw01@gmail.com>
2022-02-16 11:07:29 +0700

    draw a path from startpoint to endpoint - list location-

--- lib/providers/data_point_provider.dart
+++ lib/providers/data_point_provider.dart
@@ -1,8 +1,18 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong/latlong.dart';
+import 'package:http/http.dart' as http;
+
+class ValidationItem {
+  final String? value;
+  final String? error;
+  ValidationItem(this.value, this.error);
+}
 
 class DataPoint {
   final String name;
@@ -15,6 +25,11 @@ class DataPoint {
   });
 }
 
+class LineString {
+  LineString(this.lineString);
+  List<dynamic> lineString;
+}
+
 List<DataPoint> dataPoint = [
   const DataPoint(
       name: 'Ho Chi Minh', latitude: 10.794606, longitude: 106.721677),
@@ -23,18 +38,95 @@ List<DataPoint> dataPoint = [
   const DataPoint(name: 'Quan 1', latitude: 10.7756587, longitude: 106.7004238)
 ];
 
+class NetworkHelper {
+  NetworkHelper(
+      {required this.startLng,
+      required this.startLat,
+      required this.endLng,
+      required this.endLat});
+
+  final String url = 'https://api.openrouteservice.org/v2/directions/';
+  final String apiKey =
+      '5b3ce3597851110001cf6248de01881e3c1a42508851f987bff20c42';
+  final String journeyMode =
+      'driving-car'; // Change it if you want or make it variable
+  final double startLng;
+  final double startLat;
+  final double endLng;
+  final double endLat;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(
+        '$url$journeyMode?api_key=$apiKey&start=$startLng,$startLat&end=$endLng,$endLat'));
+
+    log("$url$journeyMode?$apiKey&start=$startLng,$startLat&end=$endLng,$endLat");
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      log(response.statusCode.toString());
+    }
+  }
+}
 
 class MapProvider with ChangeNotifier {
+  final _getAddress = TextEditingController();
+
+  TextEditingController get addressController => _getAddress;
+  String get textAddress => addressController.text;
+
+  void clearGetAddress() {
+    addressController.clear();
+    notifyListeners();
+  }
+
   MapController mapController = MapController();
-  LatLng point = LatLng(10.794606, 106.721677);
+  LatLng? point;
   LatLng? destination;
   List<Address> location = [];
   double zoomMap = 16.0;
-  
+  //-------------------------
+  final List<LatLng> polyPoints = [];
+  var data;
+
+  double startLat = 10.841088;
+  double startLng = 106.809172;
+  double endLat = 10.837543;
+  double endLng = 106.730032;
+
+  void getJsonData() async {
+    polyPoints.clear();
+
+    log("**********" + point.toString());
+
+    NetworkHelper network = NetworkHelper(
+      startLat: point == null ? startLat : point!.latitude,
+      startLng: point == null ? startLng : point!.longitude,
+      endLat: destination == null ? endLat : destination!.latitude,
+      endLng: destination == null ? endLng : destination!.longitude,
+    );
+
+    try {
+      data = await network.getData();
+      LineString ls =
+          LineString(data['features'][0]['geometry']['coordinates']);
+
+      for (int i = 0; i < ls.lineString.length; i++) {
+        polyPoints.add(LatLng(ls.lineString[i][1], ls.lineString[i][0]));
+      }
+
+      if (polyPoints.length == ls.lineString.length) {
+        log(ls.toString());
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
   Future<void> updatePosition() async {
     Position pos = await _determinePosition();
-      point = LatLng(pos.latitude, pos.longitude);
-      mapController.move(LatLng(point.latitude, point.longitude), zoomMap);
+    point = LatLng(pos.latitude, pos.longitude);
+    mapController.move(LatLng(point!.latitude, point!.longitude), zoomMap);
   }
 
   Future<Position> _determinePosition() async {
--- lib/view/google_map/google_map.dart
+++ lib/view/google_map/google_map.dart
@@ -20,71 +20,100 @@ class _GoogleMapState extends State<GoogleMap> {
   @override
   void initState() {
     super.initState();
-    dataPoint.forEach((value) => {cities.add(value.name)});
+    for (DataPoint value in dataPoint) {
+      cities.add(value.name);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     MapProvider mapProvider = Provider.of<MapProvider>(context);
+
+    onTapDestination(p) {
+      for (var i = 0; i < dataPoint.length; i++) {
+        if (p == dataPoint[i].name) {
+          setState(() {
+            mapProvider.destination =
+                LatLng(dataPoint[i].latitude, dataPoint[i].longitude);
+            mapProvider.addressController.text = dataPoint[i].name;
+            mapProvider.mapController.move(
+              LatLng(dataPoint[i].latitude, dataPoint[i].longitude),
+              mapProvider.zoomMap,
+            );
+          });
+        }
+      }
+      mapProvider.getJsonData();
+    }
+
     return Scaffold(
       body: Stack(
         children: [
-          FlutterMap(
-            mapController: mapProvider.mapController,
-            options: MapOptions(
-              onTap: (p) async {
-                List<Address> tmp = [];
-                tmp = await Geocoder.local.findAddressesFromCoordinates(
-                    Coordinates(p.latitude, p.longitude));
-                setState(() {
-                  mapProvider.point = p;
-                  mapProvider.location = tmp;
-                  mapProvider.mapController.move(
-                      LatLng(mapProvider.point.latitude,
-                          mapProvider.point.longitude),
-                      mapProvider.zoomMap);
-                });
-              },
-              center: mapProvider.point,
-              zoom: mapProvider.zoomMap,
-              minZoom: 0.0,
-              maxZoom: 18.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
+          Column(children: [
+            Flexible(
+                child: FlutterMap(
+              mapController: mapProvider.mapController,
+              options: MapOptions(
+                onTap: (p) async {
+                  List<Address> tmp = [];
+                  tmp = await Geocoder.local.findAddressesFromCoordinates(
+                      Coordinates(p.latitude, p.longitude));
+                  setState(() {
+                    mapProvider.point = p;
+                    mapProvider.location = tmp;
+                    mapProvider.mapController.move(
+                        LatLng(mapProvider.point!.latitude,
+                            mapProvider.point!.longitude),
+                        mapProvider.zoomMap);
+                  });
+                },
+                center: LatLng(mapProvider.startLat, mapProvider.startLng),
+                zoom: 16.0,
               ),
-              MarkerLayerOptions(
-                markers: [
-                  Marker(
-                    width: 50,
-                    height: 50,
-                    point: mapProvider.point,
-                    builder: (ctx) => const SizedBox(
-                      child: Icon(
-                        Icons.location_on,
-                        color: Colors.red,
+              layers: [
+                TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 50,
+                      height: 50,
+                      point: mapProvider.point,
+                      builder: (ctx) => const SizedBox(
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                        ),
                       ),
                     ),
-                  ),
-                  Marker(
-                    width: 50,
-                    height: 50,
-                    point: mapProvider.destination,
-                    builder: (ctx) => const SizedBox(
-                      child: Icon(
-                        Icons.directions_car,
-                        color: Colors.red,
+                    Marker(
+                      width: 50,
+                      height: 50,
+                      point: mapProvider.destination,
+                      builder: (ctx) => const SizedBox(
+                        child: Icon(
+                          Icons.directions_car,
+                          color: Colors.red,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                  ],
+                ),
+                PolylineLayerOptions(
+                  polylines: [
+                    Polyline(
+                        points: mapProvider.polyPoints,
+                        strokeWidth: 4.0,
+                        color: Colors.red),
+                  ],
+                ),
+              ],
+            )),
+          ]),
           Padding(
             padding:
                 const EdgeInsets.symmetric(horizontal: 16.0, vertical: 34.0),
@@ -95,13 +124,20 @@ class _GoogleMapState extends State<GoogleMap> {
                   margin: EdgeInsets.only(
                       left: 16.0, top: size.height * 0.2, right: 16.0),
                   child: SearchField(
+                    controller: mapProvider.addressController,
                     hint: "Where are you going to?",
-                    searchInputDecoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(16.0),
+                    searchInputDecoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(16.0),
+                      suffixIcon: mapProvider.addressController.text.isNotEmpty
+                          ? IconButton(
+                              icon: const Icon(Icons.close),
+                              onPressed: () => mapProvider.clearGetAddress(),
+                            )
+                          : null,
                     ),
                     maxSuggestionsInViewPort: 5,
                     suggestions: cities,
-                    onTap: (a) {},
+                    onTap: onTapDestination,
                   ),
                 ),
                 Card(
@@ -110,7 +146,7 @@ class _GoogleMapState extends State<GoogleMap> {
                     child: Column(
                       children: [
                         Text(
-                            "${mapProvider.location.isNotEmpty ? mapProvider.location.first.countryName : ""},${mapProvider.location.isNotEmpty ? mapProvider.location.first.locality : ""},${mapProvider.location.isNotEmpty ? mapProvider.location.first.featureName : ""}")
+                            "${mapProvider.location.isNotEmpty ? mapProvider.location.first.countryName : ""},${mapProvider.location.isNotEmpty ? mapProvider.location.first.featureName : ""}")
                       ],
                     ),
                   ),
